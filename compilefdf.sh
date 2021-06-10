6b8805d59616e71712ac0b91adec8d813bb56a7c
Ravindra Shinde <neelravi@gmail.com>
2021-06-10 11:32:44 +0200

    verbose mpi debugging on ccpgate

--- compilefdf.sh
+++ compilefdf.sh
@@ -7,3 +7,4 @@ make sample
 echo " mpirun -np 4 ./sample "
 echo " ___________________________________________________________________ "
 mpirun -np 4 ./sample
+#mpirun -print-all-exitcodes  -machinefile hostfile -np 30 ./sample
